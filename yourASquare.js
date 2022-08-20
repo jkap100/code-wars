b7c1e83657345bc79cc028c03b9dee9f41e3c317
Jon Kaplon <jon.kaplon1@gmail.com>
2022-08-20 11:33:10 -0700

    yourASquare

--- yourASquare.js
+++ yourASquare.js
@@ -24,6 +24,12 @@ const isSquare = (n) => {
   return m === n;
 };
 
+// const isSquare = n => Number.isInteger(Math.sqrt(n));
+
+// function isSquare(n) {
+//     return Math.sqrt(n) % 1 === 0;
+//   }
+
 const n = 25;
 
 console.log(isSquare(n));
